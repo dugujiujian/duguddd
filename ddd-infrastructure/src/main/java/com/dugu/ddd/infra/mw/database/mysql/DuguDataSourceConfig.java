a1033cd1e0a82e5b2a2a871b165e365666a616e1
刺魂 <zhaohaihua@yundongcloud.com>
2023-09-10 20:55:10 +0800

    feat:数据库处理

--- ddd-infrastructure/src/main/java/com/dugu/ddd/infra/mw/database/mysql/DuguDataSourceConfig.java
+++ ddd-infrastructure/src/main/java/com/dugu/ddd/infra/mw/database/mysql/DuguDataSourceConfig.java
@@ -1,10 +1,12 @@
 package com.dugu.ddd.infra.mw.database.mysql;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.baomidou.mybatisplus.core.MybatisConfiguration;
 import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -31,9 +33,12 @@ public class DuguDataSourceConfig {
     private String password;
     @Value("${spring.datasource.dugu.driver-class-name}")
     private String className;
+    @Autowired
+    private MybatisConfiguration mybatisConfiguration;
 
     @Primary
     @Bean(name = "duguDataSource")
+    //@ConfigurationProperties(prefix = "spring.datasource.dugu")
     public DataSource duguDataSource() {
         DruidDataSource druidDataSource = new DruidDataSource();
         druidDataSource.setUrl(url);
@@ -50,6 +55,7 @@ public class DuguDataSourceConfig {
     public SqlSessionFactory duguSqlSessionFactory(@Qualifier("duguDataSource") DataSource dataSource) throws Exception {
         MybatisSqlSessionFactoryBean bean = new MybatisSqlSessionFactoryBean();
         bean.setDataSource(dataSource);
+        bean.setConfiguration(mybatisConfiguration);
         //设置数据源
         bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/dugu/*xml"));
         //这个的getResources指向的是你的mapper.xml文件，相当于在yml中配置的mapper-locations，此处配置了yml中就不用配置，或者说不会读取yml中的该配置。
--- ddd-start/src/main/java/com/dugu/ddd/Application.java
+++ ddd-start/src/main/java/com/dugu/ddd/Application.java
@@ -1,13 +1,14 @@
 package com.dugu.ddd;
 
 
+import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author cihun
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {MybatisPlusAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
