55a1ec57814b936ec356cfa2d3f4e1d792ca3dca
刺魂 <zhaohaihua@yundongcloud.com>
2023-09-17 13:44:10 +0800

    feat:docker

--- ddd-start/src/main/java/com/dugu/ddd/Application.java
+++ ddd-start/src/main/java/com/dugu/ddd/Application.java
@@ -4,11 +4,12 @@ package com.dugu.ddd;
 import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author cihun
  */
-@SpringBootApplication(exclude = {MybatisPlusAutoConfiguration.class})
+@SpringBootApplication(exclude = {MybatisPlusAutoConfiguration.class, DataSourceAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
