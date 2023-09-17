ce1d419d22851df8e2e309443ae73feaa86579aa
刺魂 <zhaohaihua@yundongcloud.com>
2023-09-17 21:21:30 +0800

    feat:docker

--- ddd-adapter/src/main/java/com/dugu/ddd/controller/main/MainController.java
+++ ddd-adapter/src/main/java/com/dugu/ddd/controller/main/MainController.java
@@ -1,5 +1,6 @@
 package com.dugu.ddd.controller.main;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author cihun
  * @date 2021-10-09 6:55 下午
  */
-@RestController
+@Controller
 public class MainController {
 
     @RequestMapping("/index")
--- ddd-start/src/main/java/com/dugu/ddd/Application.java
+++ ddd-start/src/main/java/com/dugu/ddd/Application.java
@@ -1,15 +1,22 @@
 package com.dugu.ddd;
 
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * @author cihun
  */
-@SpringBootApplication(exclude = {MybatisPlusAutoConfiguration.class, DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {MybatisPlusAutoConfiguration.class,
+        DataSourceAutoConfiguration.class,
+        XADataSourceAutoConfiguration.class,
+        DruidDataSourceAutoConfigure.class})
+@EnableWebMvc
 public class Application {
 
     public static void main(String[] args) {
